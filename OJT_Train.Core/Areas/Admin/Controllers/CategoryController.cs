7c9a998fb0cdcc2972c9b1d244f7cad42a23b051
ANH THU <ANH THU@MSI>
2024-01-01 19:26:43 +0700

    Admin create kendo Category

---
+++ OJT_Train.Core/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories.Dto;
+using Repositories.Interfaces;
+
+namespace OJT_Train.Core.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _repo;
+        public CategoryController(ICategoryRepository repo)
+        {
+            _repo = repo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetCategory()
+        {
+            var listCategory = await _repo.GetAll();
+            return Json(listCategory);
+        }
+        public async Task<IActionResult> CreateCategory(CategoryDTO cate)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
--- OJT_Train.Core/Areas/Admin/Controllers/ManagerController.cs
+++ OJT_Train.Core/Areas/Admin/Controllers/ManagerController.cs
@@ -6,14 +6,9 @@ namespace OJT_Train.Core.Areas.Admin.Controllers
     [Area("Admin")]
     public class ManagerController : Controller
     {
-        private readonly ICategoryRepository _repo;
-        public ManagerController(ICategoryRepository repo)
-        {
-            _repo = repo;
-        }
+        
         public async Task<IActionResult> Index()
         {
-            var listCategory = await _repo.GetAll();
             return View();
         }
     }
